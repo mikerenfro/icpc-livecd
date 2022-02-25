b5a10e5750e1f75816c9889c6a040baaa2182998
Mike Renfro <renfro@tntech.edu>
2022-02-24 22:06:09 -0600

    Add variables

--- build.sh
+++ build.sh
@@ -3,46 +3,55 @@ set -e
 
 # Reference: https://askubuntu.com/a/49679
 
-ISO_URL=https://cdimage.ubuntu.com/xubuntu/releases/20.04/release/xubuntu-20.04.4-desktop-amd64.iso
+MAJOR_RELEASE=20.04
+MINOR_RELEASE=4
 WORKDIR=~/icpc-livecd
+ORIG_CD=${WORKDIR}/original-cd
+NEW_CD=${WORKDIR}/cd
+CUSTOM=${WORKDIR}/custom
+SQUASHFS=${WORKDIR}/squashfs
+
+ISO_URL=https://cdimage.ubuntu.com/xubuntu/releases/${MAJOR_RELEASE}/release/xubuntu-${MAJOR_RELEASE}.${MINOR_RELEASE}-desktop-amd64.iso
 
 # Preparations
 mkdir -p ${WORKDIR} && pushd ${WORKDIR}
-mkdir -p original-cd cd custom squashfs
+mkdir -p ${ORIG_CD} ${NEW_CD} ${CUSTOM} ${SQUAHSFS}
 wget --mirror --no-directories ${ISO_URL}
 ISO_NAME=$(basename ${ISO_URL})
-mount -o loop ${ISO_NAME} original-cd
-rsync --exclude=/casper/filesystem.squashfs -a original-cd/ cd
-mount -t squashfs -o loop original-cd/casper/filesystem.squashfs squashfs
-cp -a squashfs/* custom
-cp /etc/{resolv.conf,hosts} custom/etc/
-mount -t proc none custom/proc
-mount -t sysfs none custom/sys
+mount -o loop ${ISO_NAME} ${ORIG_CD}
+rsync --exclude=/casper/filesystem.squashfs -a ${ORIG_CD}/ ${NEW_CD}
+mount -t squashfs -o loop ${ORIG_CD}/casper/filesystem.squashfs ${SQUAHSFS}
+cp -a ${SQUAHSFS}/* custom
+cp /etc/{resolv.conf,hosts} ${CUSTOM}/etc/
+mount -t proc none ${CUSTOM}/proc
+mount -t sysfs none ${CUSTOM}/sys
 
 # Customizing
-chroot custom add-apt-repository -y ppa:deadsnakes/ppa
-chroot custom apt -y upgrade
-chroot custom apt -y install build-essential emacs vim openjdk-17-jdk-headless python2.7 python3.5
+chroot ${CUSTOM} add-apt-repository -y ppa:deadsnakes/ppa
+chroot ${CUSTOM} apt -y upgrade
+chroot ${CUSTOM} apt -y install build-essential emacs vim openjdk-17-jdk-headless python2.7 python3.5
 
 # Cleaning up
-chroot custom apt -y clean
-umount custom/proc
-umount custom/sys
-rm -rf custom/tmp/*
-rm -rf custom/etc/{resolv.conf,hosts}
+chroot ${CUSTOM} apt -y clean
+umount ${CUSTOM}/proc
+umount ${CUSTOM}/sys
+rm -rf ${CUSTOM}/tmp/*
+rm -rf ${CUSTOM}/etc/{resolv.conf,hosts}
 
 # Setting up the ISO
-chmod +w cd/casper/filesystem.manifest
-sudo chroot custom dpkg-query -W --showformat='${Package} ${Version}\n' > cd/casper/filesystem.manifest
-sudo cp cd/casper/filesystem.manifest cd/casper/filesystem.manifest-desktop
-mksquashfs custom cd/casper/filesystem.squashfs
-rm -f cd/md5sum.txt
-(cd cd && find . -type f -exec md5sum {} + > md5sum.txt)
+chmod +w ${NEW_CD}/casper/filesystem.manifest
+sudo chroot ${CUSTOM} dpkg-query -W --showformat='${Package} ${Version}\n' > ${NEW_CD}/casper/filesystem.manifest
+sudo cp ${NEW_CD}/casper/filesystem.manifest ${NEW_CD}/casper/filesystem.manifest-desktop
+mksquashfs ${CUSTOM} ${NEW_CD}/casper/filesystem.squashfs
+rm -f ${NEW_CD}/md5sum.txt
+(cd ${NEW_CD} && find . -type f -exec md5sum {} + > md5sum.txt)
 
 # Creating the ISO
-cd cd
-mkisofs -r -V "ICPC Live" -b isolinux/isolinux.bin -c isolinux/boot.cat -cache-inodes -J -l -no-emul-boot -boot-load-size 4 -boot-info-table -o ${WORKDIR}/icpc-live.iso .
+cd ${NEW_CD}
+MKISOFS_OPTIONS="-b isolinux/isolinux.bin -c isolinux/boot.cat -cache-inodes -J -l -no-emul-boot -boot-load-size 4 -boot-info-table"
+mkisofs -r -V "ICPC Live" ${MKISOFS_OPTIONS} -o ${WORKDIR}/icpc-live.iso .
 
 # Unmount and Clean
 umount ${WORKDIR}/squashfs/
 umount ${WORKDIR}/original-cd/
+popd
