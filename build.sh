76441d1bd693924565a851c320b57fbe486ba2c7
Mike Renfro <renfro@tntech.edu>
2022-02-27 18:15:36 -0600

    Deferring printing for now

--- build.sh
+++ build.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 set -e
 
-# References:
-
 WORKDIR=~/icpc
 PACKAGES="build-essential emacs neovim code openjdk-17-jdk-headless python2.7 python3.5"
 ECLIPSE_RELEASE=2021-12
 SSH_KEY_TYPE=ssh-rsa
 SSH_PUBLIC_KEY=AAAAB3NzaC1yc2EAAAADAQABAAABAQDklBg0kiico24UvcyjAcRl3ljLJasKMX5hQAGNdMLklv2gpd1hIRHE1CLP1nK7Wjj/6Cp22sQJu6kOToxstzPyKP6eDebf4rU47AJiaER3AqFBLaXtvpcyB2qRhke6uqDHvbfQwXlO/SX+OzNF7jvKAaclfw5z8Kh3HGOjr4kjMoGdZPblF8jaEKXbCYVe+HBOcJPnVuKBP8qRsxZM9nHCk3mvFlJUT5ru4Enb/wcOdsic7oUCDSxItdPJmXN8hJ1K8SJmAelT01uvY/Tp0MraCvC+j3UgSQkpgkOUpEBmzUC435NC1vt0G4TxmmfuOgZEJ2EiBnw+/ZriQqYnk8D7
 SQUID_IP=68.66.205.120
-PRINTER_IP=192.168.252.5
-PRINTER_PORT=631
+PRINTER_IP=192.168.252.5 # not yet working
+PRINTER_PORT=631 # not yet working
 
 # Hopefully nothing to change below this line
 
