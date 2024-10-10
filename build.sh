ea5348ba30a13548559194719c896eef034770dd
Mike Renfro <renfro@tntech.edu>
2024-10-10 10:29:28 -0500

    add more updates for Debian 12

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 
 WORKDIR=~/icpc
-PACKAGES="build-essential emacs neovim code openjdk-17-jdk-headless python2.7 python3.9"
+PACKAGES="gcc-11 g++-11 build-essential emacs neovim code openjdk-17-jdk-headless pypy3"
 ECLIPSE_RELEASE=2023-12
 PYCHARM_RELEASE=2023.3.3
 
@@ -45,8 +45,8 @@ gpg --dearmor < microsoft.asc > ${TRUSTED_GPG_DIR}/packages.microsoft.gpg
 # Preparations
 mkdir -p ${WORKDIR} && pushd ${WORKDIR}
 lb config \
-    --distribution bookworm \
-    --bootappend-live "boot=live components quiet splash noroot toram" \
+    --distribution bullseye \
+    --bootappend-live "boot=live components quiet splash noroot toram nouveau.modeset=0" \
     --bootappend-live-failsafe "boot=live components memtest noapic noapm nodma nomce nolapic nomodeset nosmp nosplash vga=788 noroot toram"
 rsync -av --progress ${OLDPWD}/debian-live/config/ config/
 
