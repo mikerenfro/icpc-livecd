c2ef9ea96ca9b093b534fadd81fb72e130585f18
Mike Renfro <renfro@tntech.edu>
2024-10-10 21:07:23 -0500

    correct Java class names

--- debian-live/config/includes.chroot/etc/skel/Desktop/test-compilers/leggjasaman_java.java
+++ debian-live/config/includes.chroot/etc/skel/Desktop/test-compilers/leggjasaman_java.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class leggjasaman {
+public class leggjasaman_java {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
