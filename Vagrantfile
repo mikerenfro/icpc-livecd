cac978483dd5fea89db6552c40f22119ced523cc
Mike Renfro <renfro@tntech.edu>
2024-10-27 14:59:03 -0500

    consolidating all Eclipse languages, updating README

--- Vagrantfile
+++ Vagrantfile
@@ -10,5 +10,11 @@ Vagrant.configure("2") do |config|
   config.vm.provision "shell", inline: <<-SHELL
     cd /vagrant
     ./build.sh
+    mv -v ~/icpc/live-image-amd64.hybrid.iso \
+      /vagrant/icpc-livecd-without-internet-amd64.hybrid.iso
+    rm -rf ~/icpc/
+    ./build.sh allow-internet
+    mv -v ~/icpc/live-image-amd64.hybrid.iso \
+      /vagrant/icpc-livecd-with-internet-amd64.hybrid.iso
   SHELL
 end
--- build.sh
+++ build.sh
@@ -2,48 +2,37 @@
 set -e
 
 WORKDIR=~/icpc
-PACKAGES="gcc-11 g++-11 build-essential emacs neovim code openjdk-17-jdk-headless pypy3"
-ECLIPSE_RELEASE=2024-12
-ECLIPSE_RELEASE_TYPE=M1
-PYCHARM_RELEASE=2024.2.3
-LICLIPSE_VERSION=11.1.0
+PYCHARM_RELEASE=2024.2.4
 
 # Hopefully nothing to change below this line
 
-ECLIPSE_URL_BASE=https://mirror.umd.edu/eclipse/technology/epp/downloads/release
-ECLIPSE_CPP_URL=${ECLIPSE_URL_BASE}/${ECLIPSE_RELEASE}/${ECLIPSE_RELEASE_TYPE}/eclipse-cpp-${ECLIPSE_RELEASE}-${ECLIPSE_RELEASE_TYPE}-linux-gtk-x86_64.tar.gz
-ECLIPSE_JAVA_URL=${ECLIPSE_URL_BASE}/${ECLIPSE_RELEASE}/${ECLIPSE_RELEASE_TYPE}/eclipse-java-${ECLIPSE_RELEASE}-${ECLIPSE_RELEASE_TYPE}-linux-gtk-x86_64.tar.gz
 PYCHARM_URL=https://download.jetbrains.com/python/pycharm-community-${PYCHARM_RELEASE}.tar.gz
-LICLIPSE_URL_BASE=https://www.mediafire.com/file_premium/yvh4pjh3viveurn/
-LICLIPSE_URL=${LICLIPSE_URL_BASE}/liclipse_${LICLIPSE_VERSION}_linux.gtk.x86_64.tar.gz
 # Dependencies
-sudo apt update
-sudo apt -y install live-build live-boot-doc live-config-doc
+sudo apt-get update
+sudo apt-get -y install live-build live-boot-doc live-config-doc zstd
 
 # IDE staging
-for U in ${ECLIPSE_CPP_URL} ${ECLIPSE_JAVA_URL} ${PYCHARM_URL} ${LICLIPSE_URL}; do
+for U in ${PYCHARM_URL}; do
     wget -q --mirror --no-directories --progress=bar:force:noscroll ${U}
 done
 
 ECLIPSE_DIR=${PWD}/debian-live/config/includes.chroot/opt/eclipse
 if [ ! -d ${ECLIPSE_DIR} ]; then
-    mkdir -p ${ECLIPSE_DIR}/{cpp,java}
-    tar --strip-components=1 -C ${ECLIPSE_DIR}/cpp \
-        -zxf $(basename ${ECLIPSE_CPP_URL})
-    tar --strip-components=1 -C ${ECLIPSE_DIR}/java \
-        -zxf $(basename ${ECLIPSE_JAVA_URL})
+    echo "Extracting Eclipse"
+    mkdir -p ${ECLIPSE_DIR}
+    tar --strip-components=1 -C ${ECLIPSE_DIR} \
+        -zxf eclipse.tgz
+else
+   echo "Eclipse already extracted, skipping"
 fi
 PYCHARM_DIR=${PWD}/debian-live/config/includes.chroot/opt/pycharm
 if [ ! -d ${PYCHARM_DIR} ]; then
+    echo "Extracting PyCharm"
     mkdir -p ${PYCHARM_DIR}
     tar --strip-components=1 -C ${PYCHARM_DIR} \
         -zxf $(basename ${PYCHARM_URL})
-fi
-LICLIPSE_DIR=${PWD}/debian-live/config/includes.chroot/opt/liclipse
-if [ ! -d ${LICLIPSE_DIR} ]; then
-    mkdir -p ${LICLIPSE_DIR}
-    tar --strip-components=1 -C ${LICLIPSE_DIR} \
-        -zxf $(basename ${LICLIPSE_URL})
+else
+    echo "PyCharm already extracted, skipping"
 fi
 
 # VS Code staging
@@ -60,9 +49,11 @@ fi
 mkdir -p ${WORKDIR} && pushd ${WORKDIR}
 lb config \
     --distribution bookworm \
+    --apt-recommends false \
     --bootappend-live "boot=live components quiet splash noroot toram nouveau.modeset=0" \
     --bootappend-live-failsafe "boot=live components memtest noapic noapm nodma nomce nolapic nomodeset nosmp nosplash vga=788 noroot toram"
-rsync -av --progress ${OLDPWD}/debian-live/config/ config/
+echo "Syncing debian-live/config contents"
+rsync -a ${OLDPWD}/debian-live/config/ config/
 
 lb build
 
