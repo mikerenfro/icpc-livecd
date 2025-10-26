c04dc67b61e481c87a8983b69cd40628a0cbe01d
Mike Renfro <mike+github@renf.ro>
2025-10-26 11:52:38 -0500

    remove unneeded Eclipse and folders from copy-ides.sh

--- debian-live/config/includes.chroot/etc/profile.d/copy-ides.sh
+++ debian-live/config/includes.chroot/etc/profile.d/copy-ides.sh
@@ -1,7 +1,6 @@
-for f in Eclipse-C++ Eclipse-Java Eclipse Kattis LiClipse PyCharm VS-Code; do
+for f in Eclipse-C++ Eclipse-Java Kattis LiClipse PyCharm VS-Code; do
     if [ ! -f ~/Desktop/${f}.desktop ]; then
         cp /usr/share/applications/${f}.desktop ~/Desktop/
     fi
 done
 chmod 755 ~/Desktop/*.desktop
-ln -s /opt/{eclipse,.p2,.eclipse} ~
